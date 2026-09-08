c43ab835f7b9d51611bd40a12588e61d7284678e
murad9981 <abdallahsarfaraz123@gmail.com>
2026-09-08 00:22:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ import joblib
 co = joblib.load('v')
 mo = joblib.load('model')
 
-t.title('sentiment_analysis')
+t.title('Sentiment Analysis')
+t.write("An App created by me to analyze people's sentiment.
+          DISCLAIMER: THIS IS JUST A PRELIMINARY EXPERIMENT, TRAINING DATA IS TOO LOW TO BE OF ANY USE IN PRACTICAL ENVIRONMENT.")
 
-text = t.text_input('enter your sentence: ')
+text = t.text_area('enter your sentence: ')
 
 if t.button('Analyse'):
     b = co.transform([text])
     p = mo.predict(b)
 
     if p[0] == 1:
-        t.write('pos')
+        t.write('😊 Positive')
     else:
-        t.write('neg')
+        t.write('😞 Negative')
