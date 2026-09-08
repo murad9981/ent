070e183bdc5517f552e330f9a5cdc300a924d1ba
murad9981 <abdallahsarfaraz123@gmail.com>
2026-09-08 00:31:48 -0700

    Change output methods for sentiment analysis results

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ t.title('Sentiment Analysis')
 t.write("An App created by me to analyze people's sentiment.\n\n"
           'DISCLAIMER: THIS IS JUST A PRELIMINARY EXPERIMENT, TRAINING DATA IS TOO LOW TO BE OF ANY USE IN PRACTICAL ENVIRONMENT.')
 
+t.divider()
+
+
 text = t.text_area('enter your sentence: ')
 
 if t.button('Analyse'):
@@ -16,6 +19,6 @@ if t.button('Analyse'):
     p = mo.predict(b)
 
     if p[0] == 1:
-        t.write('😊 Positive')
+        t.success("😊 Positive")
     else:
-        t.write('😞 Negative')
+        t.error('😞 Negative')
